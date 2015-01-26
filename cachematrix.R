bdb43fe6ab59dba748ef4db3427cd6a63b4d053e
kennedy906 <maybeeandrew@gmail.com>
2015-01-25 19:15:23 -0500

    Update cachematrix.R
    Completed solution for Programming Assignment 2

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,36 @@
-## Put comments here that give an overall description of what your
-## functions do
+## This exercise demonstrates the use of lexical scoping rules to pass
+##   information between different environments: in this case, storing
+##   the inverse of a matrix and recalling it from the cache later
 
-## Write a short comment describing this function
+## This abstract object stores the matrix itself along with the computed
+##   value of the inverted matrix (so that it can be recalled later)
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  inv <- NULL
+  set <- function(y) {
+    x <<- y
+    inv <<- NULL
+  }
+  get <- function() x
+  setinv <- function(inverse) inv <<- inverse
+  getinv <- function() inv
+  list(set = set, get = get,
+       setinv = setinv,
+       getinv = getinv)
 }
 
 
-## Write a short comment describing this function
+## This function receives the object created by the makeCacheMatrix function
+##   and reports the inverse, if already computed, or computes it if not.
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  inv <- x$getinv()
+  if(!is.null(inv)) {
+    message("getting cached data")
+    return(inv)
+  }
+  data <- x$get()
+  inv <- solve(data, ...)
+  x$setinv(inv)
+  inv
 }
